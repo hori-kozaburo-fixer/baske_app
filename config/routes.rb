ed49100aded878e1355816e61f932b75ea2d0c21
zab3 <xelvis03@gmail.com>
2020-10-19 15:40:42 +0900

    IDを持たせてdiscussionルーティング定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root  'home#index'
-  resources :tweets, only: [:index, :new, :create]
+  resources :tweets, only: [:index, :new, :create] do
+    member do
+      get 'discussion'
+    end
+  end
+  
 end
