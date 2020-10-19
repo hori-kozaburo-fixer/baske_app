564887641900b32fd9ba28793c6e5fcb659aa007
zab3 <xelvis03@gmail.com>
2020-10-19 12:20:13 +0900

    アクティブハッシュteamモデルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  belongs_to_active_hash :team
+  validates :team_id, numericality: { other_than: 1 }
 end
