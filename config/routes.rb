a6c75677231dd295064b0adcaf9234e259a49cbb
zab3 <xelvis03@gmail.com>
2020-10-20 13:48:07 +0900

    tweetにネストでtweet_commentsルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'tweet_comments/create'
   devise_for :users
   root  'home#index'
   resources :users, only:[:show]
@@ -12,6 +13,7 @@ Rails.application.routes.draw do
     member do
       get 'discussion'
     end
+    resources :tweet_comments, only: [:create]
   end
   
 end
