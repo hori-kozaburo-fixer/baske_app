cba34d2c29b377021a49b86a23704518b1a4e7dc
zab3 <xelvis03@gmail.com>
2020-10-20 12:14:08 +0900

    ホームコントローラーにchatアクションを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root  'home#index'
-  resources :tweets, only: [:index, :new, :create] do
+  resources :users, only:[:show]
+
+  resources :home, only:[:index] do
+    collection do
+      get 'chat'
+    end
+  end
+  resources :tweets, only: [:index, :new, :create, :show] do
     member do
       get 'discussion'
     end
