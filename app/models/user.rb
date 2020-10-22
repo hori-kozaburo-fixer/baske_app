9a5479f75feafc7b698214f06f28a936e679c9d2
zab3 <xelvis03@gmail.com>
2020-10-22 12:24:35 +0900

    ユーザーモデルの単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,9 +15,11 @@ class User < ApplicationRecord
   has_many :orders
   belongs_to_active_hash :team
 
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "は半角英数字混合で入力してください" }
+
   with_options presence: true do
     validates :nickname
-    validates :phone, format: { with: /\A\d{7,11}\z/, message: "を数字のみで入力してください" }
+    validates :phone, format: { with: /\A\d{10,11}\z/, message: "を半角数字のみ(10文字から11文字)で入力してください" }
     validates :full_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "を全角で入力してください" }
     validates :full_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "を全角（カナ）で入力してください" }
     validates :birth_day
