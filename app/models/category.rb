9762f78507dd9d1f37550403a97fa7b4d42102f4
zab3 <xelvis03@gmail.com>
2020-10-24 17:41:14 +0900

    アクティブハッシュモデルを生成

---
+++ app/models/category.rb
@@ -0,0 +1,21 @@
+class Category < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---'},
+    { id: 2, name: 'シューズ' },
+    { id: 3, name: 'バスケットウェアー'},
+    { id: 4, name: 'ソックス'},
+    { id: 5, name: 'トレーニングウェアー'},
+    { id: 6, name: 'Bリーググッズ'},
+    { id: 7, name: 'NBAグッズ'},
+    { id: 8, name: 'レフリー'},
+    { id: 9, name: 'ボール' },
+    { id: 10, name: 'コート用品'},
+    { id: 11, name: 'バッグ'},
+    { id: 12, name: 'アクセサリー・グッズ'},
+    { id: 13, name: 'インナーウェアー'},
+    { id: 14, name: 'サポーター' },
+    { id: 15, name: 'テーピング'},
+    { id: 16, name: 'アイシンググッズ'},
+    { id: 17, name: 'トレーニング用品'},
+  ]
+end
